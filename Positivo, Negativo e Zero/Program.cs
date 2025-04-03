c157bbb8cf64b126b9fceacf84a23479ad12c00a
J.U.!.C.£ <joaovitor20071220@gmail.com>
2025-04-03 10:33:00 -0300

    Ate a 5

---
+++ Positivo, Negativo e Zero/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Fernanda França
+10:27 AM(5 minutes ago)
+to marquessilvaluana22, leticiaalima77, me
+
+class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Digite um número: ");
+        int numero = Convert.ToInt32(Console.ReadLine());
+
+        if (numero > 0)
+            Console.WriteLine("O número é positivo.");
+        else if (numero < 0)
+            Console.WriteLine("O número é negativo.");
+        else
+            Console.WriteLine("O número é zero.");
+    }
