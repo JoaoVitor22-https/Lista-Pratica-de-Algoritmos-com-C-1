37c6322b006a72fcef293b0b722882d076e5e6b0
J.U.!.C.£ <joaovitor20071220@gmail.com>
2025-04-03 10:23:43 -0300

    ate 3

---
+++ Atividades/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    public static void Main()
+    {
+
+        int num;
+
+        Console.WriteLine("Escreva um numuero: ");
+        string info = Console.ReadLine();
+        num = Convert.ToInt32(info);
+
+        if (num % 2 == 0)
+        {
+            Console.WriteLine("É par");
+        }
+        else
+        {
+            Console.WriteLine("É impar");
+        }
+                     
+              
+    }
+}
